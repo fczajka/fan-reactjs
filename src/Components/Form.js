8b289733ea9de15dfa3d95511e69f55b4b542460
fczajka <franekczajkaa@gmail.com>
2022-10-07 19:57:03 +0200

    Add fetch for API requests

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,24 +1,48 @@
+import { useState, useRef } from "react";
+
 function Form() {
+    const [food, setFood] = useState([]);
+
+    const input = useRef();
+
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const API_URL = "https://api.nal.usda.gov";
+
+    async function requestFood() {
+        const data = await fetch(
+            `${API_URL}/fdc/v1/foods/search?query=${input.current.value}&dataType=Survey%20%28FNDDS%29&pageSize=10&pageNumber=1&sortBy=dataType.keyword&api_key=${API_KEY}`
+        );
+        const cleanedData = await data.json();
+
+        setFood(cleanedData.foods);
+    }
+
+    console.log(food);
+
     return (
         <div className="w-full flex justify-center pt-16">
-            <div>
-                <label className="text-purple-900" htmlFor="food">
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    requestFood();
+                }}
+            >
+                <label className="flex flex-col text-purple-900" htmlFor="food">
                     Food name:
-                </label>
-                <div>
                     <input
-                        className="bg-purple-200 rounded-l-lg p-1 mr-1"
+                        ref={input}
+                        className="w-full bg-purple-200 rounded-lg p-1 px-4"
                         type="text"
                         id="food"
                     />
-                    <button
-                        className="text-purple-50 bg-purple-700 rounded-r-lg p-1 ml-1 transition-all hover:bg-purple-800"
-                        type="submit"
-                    >
-                        CHECK
-                    </button>
-                </div>
-            </div>
+                </label>
+                <button
+                    className="w-full text-purple-50 bg-purple-700 rounded-lg p-2 mt-4 transition-all hover:bg-purple-800"
+                    type="submit"
+                >
+                    CHECK
+                </button>
+            </form>
         </div>
     );
 }
