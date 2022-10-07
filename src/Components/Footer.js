348d6aae74d15501612a558edea90db74717a94f
fczajka <franekczajkaa@gmail.com>
2022-10-07 17:24:01 +0200

    Add footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,15 @@
 function Footer() {
-    return <div></div>;
+    return (
+        <div className="w-full flex justify-center items-center h-16 bg-green-800 lg:h-20">
+            <p className="text-green-50 text-lg font-lato lg:text-xl">
+                Data provided by: U.S. Department of Agriculture, Agricultural
+                Research Service. FoodData Central, 2019.
+                <a href="fdc.nal.usda.gov" target="_blank">
+                    fdc.nal.usda.gov
+                </a>
+            </p>
+        </div>
+    );
 }
 
 export default Footer;
