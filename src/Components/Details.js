87215d203663c737ba76177be6933bb6446e4881
fczajka <franekczajkaa@gmail.com>
2022-10-09 14:45:09 +0200

    Add buttons in details

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { BsCalculator } from "react-icons/bs";
+import { RiArrowGoBackFill } from "react-icons/ri";
 
 function Details() {
     const location = useLocation();
@@ -48,7 +51,7 @@ function Details() {
             <h2 className="font-bold pt-8 pb-4 text-lg lg:text-xl lg:pt-16">
                 {data.description}
             </h2>
-            <table>
+            <table className="w-72 lg:w-96">
                 <thead className="text-md lg:text-lg">
                     <tr className="bg-green-600 text-green-50">
                         <th className="p-2 text-left">Name</th>
@@ -75,6 +78,17 @@ function Details() {
                     ))}
                 </tbody>
             </table>
+            <div className="w-72 flex mt-4 text-green-50 text-sm lg:text-md lg:w-96">
+                <button className="basis-3/4 flex justify-center items-center mr-2 p-2 lg:p-4 bg-purple-600 transition-all hover:bg-purple-700">
+                    Add to calculator{<BsCalculator />}
+                </button>
+                <Link
+                    to="/"
+                    className="basis-1/4 flex flex-col items-center ml-2 p-2 lg:p-4 bg-green-600 transition-all hover:bg-green-700"
+                >
+                    Go back{<RiArrowGoBackFill />}
+                </Link>
+            </div>
         </div>
     );
 }
