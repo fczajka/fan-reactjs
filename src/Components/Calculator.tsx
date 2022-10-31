6b973e4fdb05a14ec1bf7fb3739fb2ff3faf421b
fczajka <franekczajkaa@gmail.com>
2022-10-31 11:35:47 +0100

    Delete div from calculator

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { useEffect, useRef } from "react";
-import { TiDelete } from "react-icons/ti";
 import { useSelector } from "react-redux";
+import { TiDelete } from "react-icons/ti";
 import { DELETE_FOOD } from "../reducers/food";
-import store, { RootState } from "../store";
+import store from "../store";
+import { RootState } from "../store";
 
 type Props = {
     isOpen: boolean;
@@ -11,7 +12,6 @@ type Props = {
 
 function Calculator({ isOpen }: Props) {
     const data = useSelector((state) => state as RootState);
-
     const vh = useRef(window.innerHeight * 0.01);
 
     useEffect(() => {
@@ -29,93 +29,61 @@ function Calculator({ isOpen }: Props) {
     }, []);
 
     return (
-        <div
-            className={`fixed w-60 top-0 z-20 h-screen transition-all ease-out duration-300 delay-100 bg-green-50 lg:w-80 border-r-2 border-green-900 ${
+        <table
+            className={`fixed w-60 top-0 z-20 h-screen overflow-hidden transition-all ease-out duration-300 delay-100 bg-green-50 border-r-2 border-green-900 lg:w-80 ${
                 isOpen ? "-left-0" : "-left-80"
             }`}
         >
-            <table className="w-full overflow-hidden">
-                <thead className="flex">
-                    <tr className="flex basis-3/5 flex-col items-start">
-                        <th className="text-base px-2 py-0.5 lg:text-lg">
-                            Name
-                        </th>
-                        {data.sum.nutrients.map((nutrient) =>
-                            nutrient.unit ? (
-                                <th
-                                    key={nutrient.name}
-                                    className="text-sm px-2 py-0.5 lg:text-base"
-                                >
-                                    {nutrient.name}
-                                </th>
-                            ) : (
-                                ""
-                            )
-                        )}
-                    </tr>
-                    <tr className="flex basis-2/5 flex-col items-end">
-                        <th className="text-base px-2 py-0.5 lg:text-lg">
-                            Sum
-                        </th>
-                        {data.sum.nutrients.map((nutrient) =>
-                            nutrient.unit ? (
-                                <th
-                                    key={nutrient.name}
-                                    className="text-sm px-2 py-0.5 lg:text-base"
-                                >
-                                    {nutrient.unit === "kcal"
-                                        ? `${Math.abs(
-                                              Math.round(nutrient.value)
-                                          )} ${nutrient.unit}`
-                                        : `${Math.abs(nutrient.value).toFixed(
-                                              2
-                                          )} ${nutrient.unit}`}
-                                </th>
-                            ) : (
-                                ""
-                            )
-                        )}
-                    </tr>
-                </thead>
-                <tbody className="w-full h-calc-mobile flex flex-col overflow-y-auto lg:h-calc-desktop">
-                    {data.sum.foods.map((foodInfo) =>
-                        foodInfo.food.map((nutrients) =>
-                            !nutrients.unit ? (
-                                <tr key={nutrients.name} className="flex">
-                                    <td className="text-sm text-right basis-full px-2 py-2 bg-green-200 lg:text-base">
-                                        {nutrients.value}
-                                    </td>
-                                </tr>
-                            ) : nutrients.name === "Grams" ? (
-                                <React.Fragment key={nutrients.name}>
-                                    <tr className="flex">
-                                        <td className="text-sm text-left basis-3/5 px-2 py-0.5 lg:text-base">
-                                            {nutrients.name}
-                                        </td>
-                                        <td className="text-sm text-right basis-2/5 px-2 py-0.5 lg:text-base">
-                                            {`${nutrients.value} ${nutrients.unit}`}
-                                        </td>
-                                    </tr>
-                                    <tr className="flex justify-center">
-                                        <td className="basis-full flex p-0">
-                                            <button
-                                                onClick={() => {
-                                                    store.dispatch(
-                                                        DELETE_FOOD(foodInfo)
-                                                    );
-                                                    console.log(foodInfo);
-                                                }}
-                                                className="flex text-sm items-center py-2 justify-center basis-full trasition-all duration-300 lg:text-base hover:bg-red-200"
-                                                aria-label="Delete food from calculator"
-                                            >
-                                                Delete {foodInfo.food[0].value}
-                                                <TiDelete />
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </React.Fragment>
-                            ) : (
-                                <tr key={nutrients.name} className="flex">
+            <thead className="flex">
+                <tr className="flex basis-3/5 flex-col items-start">
+                    <th className="text-base px-2 py-0.5 lg:text-lg">Name</th>
+                    {data.sum.nutrients.map((nutrient) =>
+                        nutrient.unit ? (
+                            <th
+                                key={nutrient.name}
+                                className="text-sm px-2 py-0.5 lg:text-base"
+                            >
+                                {nutrient.name}
+                            </th>
+                        ) : (
+                            ""
+                        )
+                    )}
+                </tr>
+                <tr className="flex basis-2/5 flex-col items-end">
+                    <th className="text-base px-2 py-0.5 lg:text-lg">Sum</th>
+                    {data.sum.nutrients.map((nutrient) =>
+                        nutrient.unit ? (
+                            <th
+                                key={nutrient.name}
+                                className="text-sm px-2 py-0.5 lg:text-base"
+                            >
+                                {nutrient.unit === "kcal"
+                                    ? `${Math.abs(
+                                          Math.round(nutrient.value)
+                                      )} ${nutrient.unit}`
+                                    : `${Math.abs(nutrient.value).toFixed(2)} ${
+                                          nutrient.unit
+                                      }`}
+                            </th>
+                        ) : (
+                            ""
+                        )
+                    )}
+                </tr>
+            </thead>
+            <tbody className="w-full h-calc-mobile flex flex-col overflow-y-auto lg:h-calc-desktop">
+                {data.sum.foods.map((foodInfo) =>
+                    foodInfo.food.map((nutrients) =>
+                        !nutrients.unit ? (
+                            <tr key={nutrients.name} className="flex">
+                                <td className="text-sm text-right basis-full px-2 py-2 bg-green-200 lg:text-base">
+                                    {nutrients.value}
+                                </td>
+                            </tr>
+                        ) : nutrients.name === "Grams" ? (
+                            <React.Fragment key={nutrients.name}>
+                                <tr className="flex">
                                     <td className="text-sm text-left basis-3/5 px-2 py-0.5 lg:text-base">
                                         {nutrients.name}
                                     </td>
@@ -123,12 +91,38 @@ function Calculator({ isOpen }: Props) {
                                         {`${nutrients.value} ${nutrients.unit}`}
                                     </td>
                                 </tr>
-                            )
+                                <tr className="flex justify-center">
+                                    <td className="basis-full flex p-0">
+                                        <button
+                                            onClick={() => {
+                                                store.dispatch(
+                                                    DELETE_FOOD(foodInfo)
+                                                );
+                                                console.log(foodInfo);
+                                            }}
+                                            className="flex text-sm items-center py-2 justify-center basis-full trasition-all duration-300 lg:text-base hover:bg-red-200"
+                                            aria-label="Delete food from calculator"
+                                        >
+                                            Delete {foodInfo.food[0].value}
+                                            <TiDelete />
+                                        </button>
+                                    </td>
+                                </tr>
+                            </React.Fragment>
+                        ) : (
+                            <tr key={nutrients.name} className="flex">
+                                <td className="text-sm text-left basis-3/5 px-2 py-0.5 lg:text-base">
+                                    {nutrients.name}
+                                </td>
+                                <td className="text-sm text-right basis-2/5 px-2 py-0.5 lg:text-base">
+                                    {`${nutrients.value} ${nutrients.unit}`}
+                                </td>
+                            </tr>
                         )
-                    )}
-                </tbody>
-            </table>
-        </div>
+                    )
+                )}
+            </tbody>
+        </table>
     );
 }
 
