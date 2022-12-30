d578901a83ca53249fee20bd13f115cfa6a28034
fczajka <franekczajkaa@gmail.com>
2022-12-30 23:42:12 +0100

    Fix scale on focus

--- src/Components/ui/Result.tsx
+++ src/Components/ui/Result.tsx
@@ -10,7 +10,7 @@ function Result({ food }: Props) {
     return (
         <li className="w-full my-2 bg-blue-100 rounded-lg text-base lg:text-lg">
             <Link
-                className="w-full block px-4 py-2 rounded-lg transition-all hover:scale-105 hover:bg-blue-200 hover:shadow-md focus:bg-blue-200 focus:shadow-md"
+                className="w-full block px-4 py-2 rounded-lg transition-all hover:scale-105 hover:bg-blue-200 hover:shadow-md focus:scale-105 focus:bg-blue-200 focus:shadow-md"
                 to="/details"
                 state={food}
             >
