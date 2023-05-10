f97ad74a4be328051b965a51dc5a509ef4318936
fczajka <franekczajkaa@gmail.com>
2023-05-10 21:48:12 +0200

    Delete redundant div in Header

--- src/Components/ui/Header.tsx
+++ src/Components/ui/Header.tsx
@@ -9,21 +9,19 @@ function Header() {
     const [windowWidth] = useState<number>(window.innerWidth);
 
     return (
-        <nav className="fixed bottom-0 w-full flex justify-center bg-secondary-50 shadow-custom z-10 lg:shadow-none lg:static">
-            <div className="relative basis-full h-16 max-w-1920 flex justify-around items-center lg:justify-center lg:h-20 lg:static">
-                <h1 className="font-secondary text-lg sm:text-2xl lg:text-3xl">
-                    FOOD AND NUTRIENTS
-                </h1>
-                <Button
-                    tabIndex={IsOpen ? -1 : 0}
-                    callback={() => SetIsOpen(!IsOpen)}
-                    aria="Open calculator"
-                    type={ButtonTypes.button}
-                    myStyle="p-3 text-sm bg-tertiary-200 hover:bg-tertiary-300 focus:bg-tertiary-300 hover:-translate-y-0.5 sm:text-base lg:absolute lg:right-12 lg:p-primary lg:bg-primary-200 lg:focus:bg-primary-300 lg:hover:bg-primary-300"
-                >
-                    {windowWidth >= 1024 ? "Open calculator" : <FaHamburger />}
-                </Button>
-            </div>
+        <nav className="fixed bottom-0 w-full h-16 flex justify-around items-center bg-secondary-50 shadow-custom z-10 lg:h-20 lg:shadow-none lg:justify-center lg:static">
+            <h1 className="font-secondary text-lg sm:text-2xl lg:text-3xl">
+                FOOD AND NUTRIENTS
+            </h1>
+            <Button
+                tabIndex={IsOpen ? -1 : 0}
+                callback={() => SetIsOpen(!IsOpen)}
+                aria="Open calculator"
+                type={ButtonTypes.button}
+                myStyle="p-3 text-sm bg-tertiary-200 hover:bg-tertiary-300 focus:bg-tertiary-300 hover:-translate-y-0.5 sm:text-base lg:absolute lg:right-12 lg:p-primary lg:bg-primary-200 lg:focus:bg-primary-300 lg:hover:bg-primary-300"
+            >
+                {windowWidth >= 1024 ? "Open calculator" : <FaHamburger />}
+            </Button>
         </nav>
     );
 }
--- src/Components/ui/Layout.tsx
+++ src/Components/ui/Layout.tsx
@@ -5,7 +5,7 @@ import { JSXChild } from "../../data/Interfaces";
 
 function Layout({ children }: JSXChild) {
     return (
-        <div className="w-full h-screen font-primary text-slate-900 flex flex-col items-center bg-secondary-50">
+        <div className="w-full min-h-screen font-primary text-slate-900 flex flex-col items-center bg-secondary-50">
             <Header />
             <WrapperModal />
             <div className="w-full mt-4 p-4 sm:p-0 sm:w-110">{children}</div>
